f8e90f2a4b6f6d948949971c7dbf1e7146ce7e3f
近藤裕斗(kyokutyo) <k.yuto.cre@gmail.com>
2014-07-01 16:24:18 +0900

    先程はテキストファイルだけでしが、rails_projectsの中のものを全部コミットします。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e924da057e0e42509d2c7c42066e849c66ddd066ce8068d02aa858b00ccd2da7aa397d38da6763e3795912a04c98e6beb2685ed4dc1fdca5efab6a4bb7c96972'
